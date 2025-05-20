9d561b4b53a41fde5cbe3bf533c8c2b043b08528
leylabbabayeva <lejlababayeva25@gmail.com>
2025-05-20 22:20:26 +0200

    Commit Initial File
    writing spring for project

---
+++ elibrarydemo/src/main/java/com/main/elibrarydemo/ElibrarydemoApplication.java
@@ -0,0 +1,13 @@
+package com.main.elibrarydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibrarydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibrarydemoApplication.class, args);
+    }
+
+}
---
+++ elibrarydemo/src/test/java/com/main/elibrarydemo/ElibrarydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.elibrarydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibrarydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
